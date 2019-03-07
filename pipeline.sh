10eb3883cce1a4df9698b4aa2fe89a60c3f81708
Prerna Jain <prernaj@gatech.edu>
2019-03-07 00:40:42 -0500

    Pipeline changed - aragorn removed

--- pipeline.sh
+++ pipeline.sh
@@ -10,11 +10,10 @@ get_input() {
                         -h : Print usage instructions"
 
   # Getopts block, will take in the arguments as inputs and assign them to variables
-        while getopts "i:o:vh" option; do
+        while getopts "i:o:h" option; do
                 case $option in
                         i) input_directory=$OPTARG;;
                         o) output_directory=$OPTARG;;
-                        v) v=1;;
                         h) echo "$usage"
                               exit 0;;
                        \?) echo "Invalid option."
@@ -60,24 +59,15 @@ make_temp(){
   #Make temp Directory
   mkdir -p temp
 
-  #Make output directory
-  mkdir -p $output_directory
-
   #Parse input directory to get list of genomes
   ls $input_directory | xargs -L1 bash -c 'a=${0%.*};ext=${0#*.};echo $a >> temp/genomes_list.txt;echo $ext >>temp/genomes_list.txt'
 
   #Export input directory and to be used within xargs commands
   export input_directory
 
-}
+  #Make output directory
+  mkdir -p $output_directory
 
-run_prodigal(){
-
-  #Make directory within temp for prodigal genes
-  mkdir temp/prodigal_results
-
-  #Run prodigal on genes
-  cat temp/genomes_list.txt | xargs -L2 bash -c 'prodigal -i $input_directory/"$0"."$1" -o temp/prodigal_results/"$0".gff -f gff'
 }
 
 run_genemark(){
@@ -116,38 +106,12 @@ run_rna (){
   mv temp/final_results/* $output_directory/
 }
 
-run_bedtools(){
-
-  #Make directory within temp for merged results
-  mkdir temp/merged_results
-
-  #Find the genes that overlap between the prodigal and genemark predictions (with a minimum fraction of overlap of 0.9)
-  cat temp/genomes_list.txt | xargs -L2 bash -c 'bedtools intersect -a temp/prodigal_results/"$0".gff -b temp/genemark_results/"$0".gff -r -f 0.90  > temp/merged_results/"$0"_prodigal_genemark.bed'
-
-  #Find the genes predicted by prodigal that are not in genemark
-  cat temp/genomes_list.txt | xargs -L2 bash -c 'bedtools intersect -a temp/prodigal_results/"$0".gff -b temp/genemark_results/"$0".gff -v  > temp/merged_results/"$0"_prodigal_only.bed'
-
-  #Find the genes predicted by genemark that are not in prodigal
-  cat temp/genomes_list.txt | xargs -L2 bash -c 'bedtools intersect -a temp/genemark_results/"$0".gff -b temp/prodigal_results/"$0".gff -v  > temp/merged_results/"$0"_genemark_only.bed'
-
-  #Merge all 3 DNA prediction files (prodigal_only, genemark_only, and prodigal_genemark)
-  cat temp/genomes_list.txt | xargs -L2 bash -c 'cat temp/merged_results/"$0"_prodigal_genemark.bed temp/merged_results/"$0"_prodigal_only.bed temp/merged_results/"$0"_genemark_only.bed | sort -n -k 4 > temp/final_results/"$0"_cds.bed'
-
-  #Get fasta file for the coding regions
-  cat temp/genomes_list.txt | xargs -L2 bash -c 'bedtools getfasta -fi $input_directory/"$0"."$1" -bed temp/final_results/"$0"_cds.bed -fo temp/final_results/"$0"_cds.fna'
-
-  #Move all results to temp directory
-  mv temp/final_results/* $output_directory
-}
-
 main(){
   get_input "$@"
   make_temp
-  #run_prodigal "$input_directory"
   run_genemark "$input_directory"
   run_rna "$input_directory"
-
-  #run_bedtools
 }
 
 main "$@"
+
